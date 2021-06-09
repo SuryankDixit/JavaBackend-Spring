e2b989eb1706dedc0dce8e6b5d9383b2092ac763
SuryankDixit <suryankdixit@gmail.com>
2021-06-09 20:03:41 +0530

    @PostConstruct and @preDestroy working

--- demo/src/main/java/com/example/demo/basic/BinarySearchImpl.java
+++ demo/src/main/java/com/example/demo/basic/BinarySearchImpl.java
@@ -1,10 +1,16 @@
 package com.example.demo.basic;
+import com.example.demo.scopeDemoApplication;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 
 @Component
 @Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
@@ -35,6 +41,9 @@ public class BinarySearchImpl {
 	@Autowired
 	@Qualifier("bubble")
 	private SortAlgorithm sortAlgorithm;
+
+	private Logger LOGGER = LoggerFactory.getLogger(this.getClass());
+
 	public int binarySearch(int[] numbers, int numberToSearchFor) {
 
 		int[] sortedNumbers = sortAlgorithm.sort(numbers);
@@ -43,4 +52,13 @@ public class BinarySearchImpl {
 		return 3;
 	}
 
+	@PostConstruct
+	public void postConstruct(){
+		LOGGER.info("postConstruct");
+	}
+
+	@PreDestroy
+	public void preDestroy(){
+		LOGGER.info("preDestroy");
+	}
 }
