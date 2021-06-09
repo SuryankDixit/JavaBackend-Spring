d9c3b034cc1f71ae219211875aad9bb4fea3d7c8
SuryankDixit <suryankdixit@gmail.com>
2021-06-09 16:54:35 +0530

    @ComponentScna usage is shown through componentscan package

--- demo/src/main/java/com/example/demo/scopeDemoApplication.java
+++ demo/src/main/java/com/example/demo/scopeDemoApplication.java
@@ -1,27 +1,35 @@
 package com.example.demo;
 
+import com.example.componentscan.ComponentDAO;
 import com.example.demo.scope.PersonDAO;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("com.example.componentscan")
 public class scopeDemoApplication {
 
     private static Logger LOGGER = LoggerFactory.getLogger(scopeDemoApplication.class);
 
     public static void main(String[] args) {
         ApplicationContext applicationContext = SpringApplication.run(basicDemoApplication.class, args);
-        PersonDAO pd1 = applicationContext.getBean(PersonDAO.class);
-        PersonDAO pd2 = applicationContext.getBean(PersonDAO.class);
+//        PersonDAO pd1 = applicationContext.getBean(PersonDAO.class);
+//        PersonDAO pd2 = applicationContext.getBean(PersonDAO.class);
+//
+//        LOGGER.info("{}",pd1);
+//        LOGGER.info("{}",pd1.getJdbcConnection());
+//        LOGGER.info("{}",pd1.getJdbcConnection());
+//
+//        LOGGER.info("{}",pd2);
+//        LOGGER.info("{}",pd2.getJdbcConnection());
 
-        LOGGER.info("{}",pd1);
-        LOGGER.info("{}",pd1.getJdbcConnection());
-
-        LOGGER.info("{}",pd2);
-        LOGGER.info("{}",pd2.getJdbcConnection());
+        // Add component package name at top  only then spring boot will search for this bean in that package
+        ComponentDAO cd = applicationContext.getBean(ComponentDAO.class);
+        LOGGER.info("{}",cd.getJdbcConnection());
     }
 
 }
