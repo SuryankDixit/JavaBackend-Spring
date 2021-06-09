18a1af3fae7d3221f80984087554100699fbb98d
SuryankDixit <suryankdixit@gmail.com>
2021-06-09 11:45:01 +0530

    Constructor and Setter Injections Comments Added

--- demo/src/main/java/com/example/demo/BinarySearchImpl.java
+++ demo/src/main/java/com/example/demo/BinarySearchImpl.java
@@ -9,12 +9,20 @@ public class BinarySearchImpl {
 	private SortAlgorithm sortAlgorithm;
 	/*
 		tut1
+		All mandatory dependency should use constructor injection
+
 		public BinarySearchImpl(SortAlgorithm sort){
-		super();
-		this.sortAlgorithm = sort;
+			super();
+			this.sortAlgorithm = sort;
 		}
-	 */
-	
+
+		All optional dependency should use setter injection
+		public void setSortAlgorithm(SortAlgorithm sortAlgorithm) {
+			this.sortAlgorithm = sortAlgorithm;
+		}
+	*/
+
+
 	public int binarySearch(int[] numbers, int numberToSearchFor) {
 
 		int[] sortedNumbers = sortAlgorithm.sort(numbers);
